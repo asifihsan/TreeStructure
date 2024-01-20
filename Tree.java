b4b69f62a9d300a9129f1d5ddaf07a5cb58177ca
Asif Ihsan <asifihsan0369@gmail.com>
2024-01-20 13:14:13 +0530

    Completed

--- Tree.java
+++ Tree.java
@@ -20,12 +20,13 @@ class Tree {
         String a = "Five:5:4,Four:4:2,Two:2:1,Three:3:2,One:null:null";
         String b[] = a.split(",");
         int t = 0;
+        int s=0;
         for (String v : b) {
             String values[] = v.split(":");
             if (!"null".equals(values[2])) {
                 n[t] = new Tstr(values[0], Integer.parseInt(values[1]), Integer.parseInt(values[2]));
             } else {
-                n[t] = new Tstr(values[0], 0, 0);
+                n[t] = new Tstr(values[0], 1, 0);
             }
             t++;
         }
@@ -39,9 +40,25 @@ class Tree {
            if(j.parent==0)
            {
             System.out.print(j.n);
+            s=s+1;
            }else{
-            System.out.print("-->");
-            System.out.print(j.n);
+           
+            if(n[j.p-2].p==j.parent){
+                System.out.print("-->");
+                System.out.print(j.n);
+                s=s+1;
+            }else{
+                System.out.println("");
+                for(int r=0;r<s;r++)
+                {
+                    System.out.print("   "); 
+                }
+                
+                System.out.print("-->");
+                System.out.print(j.n); 
+            }
+                // System.err.println(n[j.p-2].p+"  "+j.parent);
+                
            }
         }
     }
